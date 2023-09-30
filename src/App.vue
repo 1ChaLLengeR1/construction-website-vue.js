a58a4d444011bef6a5805236a5c67c6ad7386c97
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2023-09-30 16:37:26 +0200

    full website construction vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="navigation__container">
+      <the-navigation></the-navigation>
+    </div>
+    <div class="pages__container">
+      <router-view></router-view>
+    </div>
+    <div class="footer__container">
+      <the-footer></the-footer>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import TheFooter from "../src/components/util/The-Footer.vue";
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import TheHeaderVue from "./components/navigation/The-Header.vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    "the-footer": TheFooter,
+    "the-navigation": TheHeaderVue,
+  },
+  setup() {
+    //values
+    const store = useStore();
+
+    //functions
+    store.dispatch("gallery/loadGalleries");
+    store.dispatch("auth/tryLogin")
+
+    return {};
+  },
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+  scroll-padding-top: 7rem;
+
+  &::-webkit-scrollbar {
+    width: 1rem;
+  }
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: var(--color-yellow);
+    border: 1px solid white;
+  }
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+:root {
+  --color-yellow: #ffd000;
+}
+
+.app {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+
+  .navigation__container {
+    width: 100%;
+    height: 5rem;
+  }
+
+  .pages__container {
+    width: 100%;
+    min-height: calc(100vh - 10rem);
+  }
+
+  .footer__container {
+    width: 100%;
+    height: 5rem;
+  }
 }
 </style>
